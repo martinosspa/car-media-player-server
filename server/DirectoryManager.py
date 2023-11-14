f593069f5e6e66b65526bf7eef5b06fc7721e5ef
martinosspa <martinosspa@gmail.com>
2023-11-14 20:47:05 +0200

    Fixed not building directories correctly

--- server/DirectoryManager.py
+++ server/DirectoryManager.py
@@ -1,14 +1,11 @@
 import os
 
-
-
 class DirectoryManager:
-
 	def __init__(self, path) -> None:
 		self._path = path
 		self._folder_list = None
 
-	def build_paths(self) -> list:
+	def build(self) -> list:
 		''' Builds a list of all directories and files in the path specified
 			This is meant to be called with Linux/Unix file structure in mind
 			TODO: Windows file structure
@@ -17,20 +14,19 @@ class DirectoryManager:
 			if this order breaks the syncing will break down, this is why the communication
 			is setup with TCP
 			'''
-		path = self._path + '/'
-		os.chdir(path)
-		local_library = build_local_library(path)
+		path = os.getcwd() + '/' + self._path
+		self._folder_list = self._build_sub_directories(self._path)
 
+	def _build_sub_directories(self, path) -> list:
 		folder_list = []
 		for file in os.listdir(path):
 			if os.path.isfile(path + file):
 				folder_list.append(path + file)
 			else:
 				folder_list.append(path + file + '/')
-				if directory_dict := build_local_library(path + file + '/'):
-					folder_list.append(*directory_dict)
-		self._folder_list = folder_list
-		return self._folder_list
+				if directory_dict := self._build_sub_directories(path + file + '/'):
+					folder_list += directory_dict # Both are lists
+		return folder_list
 
 	def get_folders(self) -> list:
 		return self._folder_list
