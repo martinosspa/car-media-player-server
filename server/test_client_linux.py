b36c3ea74f2a6a174a10c1c20a098abfa660bc91
martinosspa <martinosspa@gmail.com>
2023-11-27 21:07:10 +0200

    Update test_client_linux.py

--- server/test_client_linux.py
+++ server/test_client_linux.py
@@ -2,6 +2,9 @@ import socket
 import time
 import os
 import json
+from ComProtocol import ComProtocol as ComProt
+# NOTE : This file doesn't have any modularity as it will become it's own repo
+# and application at time some, this should only be used for testing
 
 def receive_file(server, file_path) -> None:
 	file = open(file_path, 'w+b')
@@ -12,22 +15,22 @@ def receive_file(server, file_path) -> None:
 		# TODO: this is hacky but works, maybe try to find a better alternative
 		# probably an EOF tag from server
 		if len(message) < 10: 
-			if message.decode() == '<EOF>':
+			if message.decode() == ComProt.EOF:
 				file.close()
-				server.sendall('<SNF>'.encode())
+				server.sendall(ComProt.SNF.encode())
 				break
 			else:
-				print(message_length)
+				print(message)
 
 def build_local_library(path:str) -> list:
 	folder_list = []
-	for file in os.listdir(path):
+	for file in os.listdir(os.getcwd() + path):
 		if os.path.isfile(path + file):
 			folder_list.append(path + file)
 		else:
-			folder_list.append(file + '/')
+			folder_list.append(path + file + '/')
 			if directory_dict := build_local_library(path + file + '/'):
-				folder_list.append(*directory_dict)
+				folder_list += directory_dict
 	return folder_list
 
 def main():
@@ -37,25 +40,32 @@ def main():
 	last_sync = str(time.time())
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((HOST, PORT))
-		s.sendall(last_sync.encode())
+		#uuid = '5a015648-ddaa-4198-bc5c-744e670a1a8b'
+		s.sendall(ComProt.NO_UUID.encode())
+		#s.sendall(uuid.encode())
+		#s.sendall(last_sync.encode())
+		uuid = s.recv(1024)
+		s.sendall('-2'.encode())
 		data = s.recv(1024)
 		decoded_data = data.decode()
 
-		if decoded_data == 'okay':
+		print(decoded_data)
+		if decoded_data ==  ComProt.OKAY:
 			# update last_sync to file
 			return
-		else:
+		elif decoded_data == ComProt.SYNC:
 			# start sync
-			path = 'audio/' # Hardcoded
+			print('started sync from server')
+			path = '/music/' # Hardcoded
 
-			temp_lib = build_local_library('audio/')
+			temp_lib = build_local_library(path)
 			local_library = json.dumps(temp_lib)
 			s.sendall(local_library.encode())
 
 
 			# receive changes
 			while True:
-				file_path = os.getcwd() + '/' + s.recv(1024).decode() # Converts it to a local path
+				file_path = os.getcwd() + s.recv(1024).decode() # Converts it to a local path
 				# data is type of current type of syncing file
 				# directory or actual file
 				if not file_path:
@@ -70,7 +80,7 @@ def main():
 						os.makedirs(file_path)
 					continue
 
-				if file_path[-5::] == '<END>':
+				if file_path[-5::] == ComProt.END:
 					print('got <END>')
 					break
 
