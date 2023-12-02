c8597cd89f4fe944282f22a4d30f9c6f24f4ca0e
martinosspa <martinosspa@gmail.com>
2023-12-02 13:59:59 +0200

    Server formatting

--- server/Server.py
+++ server/Server.py
@@ -2,10 +2,9 @@ import socket
 import logging
 from ClientHandler import ClientHandler
 from DirectoryManager import DirectoryManager
-from ComProtocol import ComProtocol as ComProt
-import time
 
 class Server:
+	'''Main Server object, opens a thread for each connection'''
 	_socket = None
 	_message_handler = None
 	_directory_manager = None
@@ -16,15 +15,11 @@ class Server:
 	def __init__(self) -> None:
 		self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-		#TODO : remove hardcoded local ip
 		self._socket.bind(('0.0.0.0', 9999))
 		logging.info('Started succesfully')
-		
 		#TODO : remove hardcoded local library lookup location
 		self._directory_manager = DirectoryManager('/music/')
 		self._directory_manager.build()
-
 		self._start_main_loop()
 
 	def _start_main_loop(self) -> None:
@@ -34,7 +29,6 @@ class Server:
 				client, addr = self._socket.accept()
 				logging.info(f'Connected by {addr[0]}:{addr[1]}')
 				self._current_clients.append(ClientHandler(client, addr, self._directory_manager))
-				
 		# TEMPORARY
 		except KeyboardInterrupt:
 			print('\n')
