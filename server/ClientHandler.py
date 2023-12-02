6c4b20e8db670ee75e2b5f197be26d6ee7039472
martinosspa <martinosspa@gmail.com>
2023-12-02 19:01:14 +0200

    removed message handler requiring client on each method call

--- server/ClientHandler.py
+++ server/ClientHandler.py
@@ -9,24 +9,24 @@ from ComProtocol import ComProtocol as ComProt
 
 THREADING_LOCK = threading.Lock()
 
+
 class ClientHandler(threading.Thread):
 	_client_uuid = None
 	_is_client_fresh = False
+
 	def __init__(self, client, addr, directory_manager) -> None:
 		super().__init__()
 		self._client = client
 		self._client_addr = addr
 		self._directory_manager = directory_manager
-		self._message_handler = MessageHandler()
-
-		#TODO : remove hardcoded client.json file
-		self._load_clients_from_json('clients.json')
+		self._message_handler = MessageHandler(self._client)
+		self._load_clients_from_json('clients.json')  # TODO : remove hardcoded client.json file
 		self.run()
 
 	def run(self) -> None:
 		self._check_if_client_is_fresh()
 		print(self._is_client_fresh)
-		
+
 		if self._is_client_fresh:
 			self._client_begin_sync(self._client_uuid)
 		else:
@@ -41,21 +41,21 @@ class ClientHandler(threading.Thread):
 			Changes the _is_client_fresh flag'''
 
 		# Client either sends a uuid message or a no_uuid message
-		uuid_message = self._message_handler.receive_from_client(self._client)
+		uuid_message = self._message_handler.receive_from_client()
 
 		if uuid_message == ComProt.UUID:
 			logging.debug(f'Client {self._client} has UUID')
 			# Client already has a UUID
 			# Return True so we skip the exchange of
 			# Last Sync
-			self._client_uuid = self._message_handler.receive_from_client(self._client)
+			self._client_uuid = self._message_handler.receive_from_client()
 			self._is_client_fresh = False
 
 		elif uuid_message == ComProt.NO_UUID:
 			logging.debug(f'Client {self._client} has no UUID')
 			# generate a uuid for the client and send it
 			self._client_uuid = str(uuid.uuid4())
-			self._message_handler.send_to_client(self._client, self._client_uuid)
+			self._message_handler.send_to_client(self._client_uuid)
 			# save client to clients file
 			self._client_data['clients'][self._client_uuid] = {'last_sync':'-1'}
 			self._save_client_data_to_file('clients.json')
@@ -63,14 +63,12 @@ class ClientHandler(threading.Thread):
 
 		else:
 			logging.error(f'Error generating UUID for client at {self._client_addr}')
-		
-		
 
 	def _handle_client_last_sync(self, client_uuid) -> None:
 		'''Handles Syncing logic'''
-		client_last_sync = self._message_handler.receive_from_client(self._client)
+		client_last_sync = self._message_handler.receive_from_client()
 		if float(client_last_sync) > float(self._client_data['clients'][client_uuid]['last_sync']):
-			self._message_handler.send_to_client(self._client, ComProt.OKAY)
+			self._message_handler.send_to_client(ComProt.OKAY)
 			logging.info(f'Client {client_uuid} is synced')			
 		else:
 			self._client_begin_sync(client_uuid)
@@ -80,8 +78,8 @@ class ClientHandler(threading.Thread):
 
 		# Next step in handshake is for client to send their library
 		logging.info(f'Client {client_uuid} needs to sync')
-		self._message_handler.send_to_client(self._client, ComProt.SYNC)
-		data = self._message_handler.receive_from_client(self._client)
+		self._message_handler.send_to_client(ComProt.SYNC)
+		data = self._message_handler.receive_from_client()
 		if data:
 			client_library_json = json.loads(data)
 		else: 
@@ -89,7 +87,7 @@ class ClientHandler(threading.Thread):
 		self._send_file_differences_to_client(client_library_json)
 
 		# End the handshake
-		self._message_handler.send_to_client(self._client, ComProt.END)
+		self._message_handler.send_to_client(ComProt.END)
 		logging.info(f'Client {client_uuid} synced')
 
 	def _send_file_differences_to_client(self, client_library) -> None:
@@ -108,23 +106,23 @@ class ClientHandler(threading.Thread):
 				# current file is a directory
 				# that client doesnt have
 				# send that difference so client can create the directory
-				self._message_handler.send_to_client(self._client, file)
+				self._message_handler.send_to_client(file)
 				time.sleep(0.05)
 				continue
 
-			self._message_handler.send_to_client(self._client, file)
+			self._message_handler.send_to_client(file)
 			time.sleep(0.05)
-			self._message_handler.send_file_to_client(self._client, file)
+			self._message_handler.send_file_to_client(file)
 			
 			# Wait for client message is a blocking method 
 			# that waits for client to confirm that a file was received
-			self._message_handler.wait_for_client_message(self._client, ComProt.SNF)
+			self._message_handler.wait_for_client_message(ComProt.SNF)
 
 	def _load_clients_from_json(self, file_name) -> None:
 		if not os.path.isfile(file_name):
 			with open(file_name, 'w') as file:
 				# check if client file exists, if not create it
-				data_preload = {'clients':{}}
+				data_preload = {'clients': {}}
 				json.dump(data_preload, file)
 
 		with open(file_name) as file:
--- server/MessageHandler.py
+++ server/MessageHandler.py
@@ -6,30 +6,34 @@ from ComProtocol import ComProtocol as ComProt
 class MessageHandler:
 	'''This class manages all communication with clients'''
 
-	def send_to_client(self, client, message) -> None:
-		client.sendall(message.encode())
-		logging.debug(f'Sent {message} to client {client}')
+	def __init__(self, client) -> None:
+		'''Message handler class takes a client as a required paramater'''
+		self._client = client
 
-	def send_file_to_client(self, client, file_path) -> None:
-		'''Sends a file to client'''
+	def send_to_client(self, message) -> None:
+		self._client.sendall(message.encode())
+		logging.debug(f'Sent {message} to client {self._client}')
+
+	def send_file_to_client(self, file_path) -> None:
+		'''Sends a file to client, needs a file path'''
 		full_path = os.getcwd() + file_path
 		with open(full_path, 'rb') as file:
 			while message := file.read(40960):
-				client.sendall(message)
+				self._client.sendall(message)
 			time.sleep(0.05)
-			client.sendall(ComProt.EOF.encode())
+			self._client.sendall(ComProt.EOF.encode())
 
-	def wait_for_client_message(self, client, message) -> None:
+	def wait_for_client_message(self, message) -> None:
 		''' This is a blocking method'''
-		logging.debug(f'Waiting for {client} to send {message}')
+		logging.debug(f'Waiting for {self._client} to send {message}')
 		while True:
-			client_message = client.recv(1024).decode()
+			client_message = self._client.recv(1024).decode()
 			if client_message == message: 
 				break
 
-	def receive_from_client(self, client, data_amount=1024) -> str:
+	def receive_from_client(self, data_amount=1024) -> str:
 		'''Receive data from client, optional data_amount with default value of 1024
 			Returns a string'''
-		data = client.recv(data_amount).decode()
-		logging.debug(f'Received {data} from {client}')
+		data = self._client.recv(data_amount).decode()
+		logging.debug(f'Received {data} from {self._client}')
 		return data
