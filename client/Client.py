3615a171dcda1ff6ca60fa3b67285c4a37fde49a
martinosspa <martinosspa@gmail.com>
2023-12-04 20:53:25 +0200

    removed old client code

--- client/Client.py
+++ client/Client.py
@@ -159,69 +159,10 @@ class Client:
 					break
 				else:
 					print(message)
-def main_old():
-	HOST = '192.168.0.120'  # The server's hostname or IP address, currently local ip address
-	PORT = 9999  # The port used by the server position
-
-	last_sync = str(time.time())
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect((HOST, PORT))
-		#uuid = '5a015648-ddaa-4198-bc5c-744e670a1a8b'
-		s.sendall(ComProt.NO_UUID.encode())
-		#s.sendall(uuid.encode())
-		#s.sendall(last_sync.encode())
-		uuid = s.recv(1024)
-		#s.sendall('-2'.encode())
-		data = s.recv(1024)
-		decoded_data = data.decode()
-
-		print(decoded_data)
-		if decoded_data ==  ComProt.OKAY:
-			# update last_sync to file
-			return
-		elif decoded_data == ComProt.SYNC:
-			# start sync
-			print('started sync from server')
-			path = '/music/' # Hardcoded
-
-			temp_lib = build_local_library(path)
-			local_library = json.dumps(temp_lib)
-			s.sendall(local_library.encode())
-			
-
-
-			# receive changes
-			while True:
-				file_path = os.getcwd() + s.recv(1024).decode() # Converts it to a local path
-				# data is type of current type of syncing file
-				# directory or actual file
-				if not file_path:
-					print('got empty message, something went wrong')
-					break
-				print(file_path)
-
-				if file_path[-1] == '/':
-					# current syncing file is a directory
-					# if it doesnt already exist, create it
-					if not os.path.isdir(file_path):
-						os.makedirs(file_path)
-					continue
-
-				if file_path[-5::] == ComProt.END:
-					print('got <END>')
-					break
-
-				
-				receive_file(s, file_path)
-
-					#print(len(message))
-
-		print('done')
-	print('closed')
-
 
 def main():
 	client = Client()
+
 if __name__ == '__main__':
 	logging.basicConfig(format='[%(asctime)s] [%(levelname)s]: %(message)s', 
 				datefmt='%d %b %H:%M:%S',
